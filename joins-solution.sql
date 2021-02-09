9312d446168854d80e4103817fb17db2b54511f2
noahwagner20 <noah.wagner1998@gmail.com>
2021-02-09 13:40:17 -0600

    fixed a little a more questions

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,14 +1,21 @@
 
     -- 1. Get all customers and their addresses.
+      SELECT (customers)id, first_name, last_name, (addresses)id, street, state, zip
+    FROM customers
+    INNER JOIN addresses
+    ON customers.id=addresses.id;
     -- 2. Get all orders and their line items (orders, quantity and product).
- SELECT (orders)id, (line_items)id, (line_items)quantity, (line_items)productid
+ SELECT (orders)id, (line_items)id, quantity, product_id
     FROM orders
     INNER JOIN line_items
 	ON orders.id=line_items.id;
     -- 3. Which warehouses have cheetos?
     -- 4. Which warehouses have diet pepsi?
     -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-    
+    SELECT (customers)id, (customers)first_name, (customers)last_name, (line_items)id, (line_items)quantity,
+    from customers
+    INNER JOIN line_items
+    ON customers.id=line_items.id;
     -- 6. How many customers do we have?
     SELECT SUM("id") FROM "customers"; 10
     -- 7. How many products do we carry?

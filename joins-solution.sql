6c3e23ce5197d1493cb819feb98415fa1caf4a23
noahwagner20 <noah.wagner1998@gmail.com>
2021-02-09 13:55:44 -0600

    base mode

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,16 +1,26 @@
 
     -- 1. Get all customers and their addresses.
-      SELECT (customers)id, first_name, last_name, (addresses)id, street, state, zip
-    FROM customers
-    INNER JOIN addresses
-    ON customers.id=addresses.id;
+      SELECT *
+    FROM customers AS c
+    JOIN addresses AS a
+    ON c.id=a.id;
     -- 2. Get all orders and their line items (orders, quantity and product).
- SELECT (orders)id, (line_items)id, quantity, product_id
-    FROM orders
-    INNER JOIN line_items
-	ON orders.id=line_items.id;
+ SELECT *
+    FROM orders as o
+    JOIN line_items as li
+	ON o.id=li.order_id;
     -- 3. Which warehouses have cheetos?
+SELECT *
+    FROM warehouse_product AS w
+    JOIN warehouse_product AS wp
+    ON w.id = wp.warehouse_id
+    WHERE wp.product.id = 5 AND wp.on_hand > 0;
     -- 4. Which warehouses have diet pepsi?
+    SELECT *
+    FROM warehouse_product AS w
+    JOIN warehouse_product AS wp
+    ON w.id = wp.warehouse_id
+    WHERE wp.product.id = 6 AND wp.on_hand > 0;
     -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
     SELECT (customers)id, (customers)first_name, (customers)last_name, (line_items)id, (line_items)quantity,
     from customers
@@ -21,7 +31,8 @@
     -- 7. How many products do we carry?
       SELECT SUM("id") FROM "products"; 28
     -- 8. What is the total available on-hand quantity of diet pepsi?
-
+    SELECT SUM(on_hand) FROM warehouse_product
+    WHERE product_id = 6;
 
 --Stretch
 
